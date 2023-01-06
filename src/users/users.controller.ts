5db120f1f350df95c6dbcbc4c9fadf98eab78256
damien-verschaere <damien.verschaere@laplateforme.io>
2023-01-06 11:58:01 +0100

    login et bcrypt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { Get } from '@nestjs/common/decorators';
+import { Controller } from '@nestjs/common';
+import { Get, Param, Post, Body, UseGuards } from '@nestjs/common/decorators';
+import { get } from 'http';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 
+
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService:UsersService){}
+  constructor(private readonly usersService: UsersService) { }
 
-    @Post('register')
-    async register(@Body() UserEntity: User){
-        return  this.usersService.create(UserEntity)
-    }
+  @Post('register')
+  async create(@Body() user: any) {
+    return this.usersService.create(user);
+  }
+  @Post('login')
+  async login(@Body() user: any) {
+    const { email, password } = user;
+    return this.usersService.login(email, password);
+  }
 
-    @Get('all')
-    findAll() {
-      return this.usersService.findAll();
-    }
+  @Get('all')
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+ 
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,56 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Body } from '@nestjs/common';
 import { EntitySchemaEmbeddedColumnOptions, Repository } from 'typeorm';
 import { User } from './users.entity';
+import * as bcrypt from 'bcrypt';
+
+
 
 @Injectable()
 export class UsersService {
- findAll(): Promise<User[]> {
-  return this.usersRepository.query('SELECT * FROM user;');
-}
+
   constructor(
     @Inject('user')
     private usersRepository: Repository<User>,
   ) {}
+  
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+  findOne(id:number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+  
+  
+  async create(userentity: User) {
+    
+    this.usersRepository.create(userentity);
+    const insertUserDatabse = this.usersRepository
 
-  async create(userentity: User): Promise<User> {
-    const createdUser = this.usersRepository.create(userentity);
-    return await createdUser;
-}
-
-
- 
+    .createQueryBuilder()
+    .insert()
+    .into(User)
+    .values([{
+     email:userentity.email,
+     name:userentity.name,
+     lastname:userentity.lastname,
+     password:await bcrypt.hash(userentity.password,10)
+    }]
+    )
+    .execute();
+    // const insertUsecreatedUser.id,createdUser.name,createdUser.lastname,createdUser.passwordrDatabse= this.usersRepository'
+    // .query('INSERT INTO user (email,name,lastname,password) Value ("createdUser.id",'createdUser.name','createdUser.lastname','createdUser.password) ')
+    await insertUserDatabse;
+  }
+  async login(email: any, password: any): Promise<{ success: boolean, message: string, user?: any }> {
+    const user = await this.usersRepository.findOneBy({email});
+    if (!user) {
+      return { success: false, message: 'Login failed. Invalid username or password.' };
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      return { success: true, message: 'Login successful.', user };
+    }
+    return { success: false, message: 'Login failed. Invalid username or password.' };
+  }
+  
 }
