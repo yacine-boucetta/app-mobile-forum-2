d87deb1d46880768a451581e962333acb6e456a3
yacine_boucetta <yacine.boucetta@laplateforme.io>
2023-07-15 17:19:14 +0200

    prod test 3

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { Picture } from "./pictures/model/entities/pictures.entity";
 import { User } from "./users/model/entities/users.entity";
 
-export const typeOrmConfig: TypeOrmModuleOptions =  {
+export const typeOrmConfig =  {
             type: 'mysql',
-            host: 'localhost',
+            host: 'https://damien-verschaere.students-laplateforme.io:8443/',
             port: 3306,
-            username: 'root',
-            password: '',
-            database: 'api_share_event',
+            username: 'RYD',
+            password: 'shareevent2023!',
+            database: 'damien-verschaere_shareEvent',
             synchronize: true,
             logging: true,
             entities: [User,Picture,Event],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { typeOrmConfig } from './config';
 
-const port = process.env.PORT || 3000;
+const port = typeOrmConfig.port || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
